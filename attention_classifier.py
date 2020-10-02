8a6e9f1096e035b04a72b7a907b33d1b457ab1f6
QiaoLiu <smileqiao1988@yahoo.com>
2020-10-01 23:01:55 -0400

    debug

--- attention_classifier.py
+++ attention_classifier.py
@@ -209,7 +209,7 @@ def run():
         oversample_train_index = list(new_train_index)
         random.shuffle(oversample_train_index)
 
-        partition = {'train': list(final_index.iloc[oversample_train_index]),
+        partition = {'train': list(final_index.iloc[train_index]),
                      'test1': list(final_index.iloc[test_index]), 'test2': list(final_index.iloc[test_index_2]),
                      'eval1': list(final_index.iloc[evaluation_index]),
                      'eval2': list(final_index.iloc[evaluation_index_2])}
@@ -256,17 +256,17 @@ def run():
                 # Model computations
                 preds = drug_model(*local_batch)
                 preds = preds.contiguous().view(-1)
-                prediction_on_cpu = preds.cpu().numpy()
-                # mean_prediction_on_cpu = np.mean([prediction_on_cpu[:sample_size],
-                #                                   prediction_on_cpu[sample_size:]], axis=0)
-                mean_prediction_on_cpu = prediction_on_cpu[:sample_size]
-                ys = local_labels.contiguous().view(-1)
                 optimizer.zero_grad()
+                ys = local_labels.contiguous().view(-1)
                 assert preds.size(0) == ys.size(0)
                 # loss = F.nll_loss(preds, ys)
                 criterion = torch.nn.BCEWithLogitsLoss()
                 loss = criterion(preds, ys.float())
                 loss.backward()
+                prediction_on_cpu = preds.cpu().numpy()
+                # mean_prediction_on_cpu = np.mean([prediction_on_cpu[:sample_size],
+                #                                   prediction_on_cpu[sample_size:]], axis=0)
+                mean_prediction_on_cpu = prediction_on_cpu[:sample_size]
                 optimizer.step()
                 all_preds.append(mean_prediction_on_cpu)
                 all_ys.append(local_labels_on_cpu)
