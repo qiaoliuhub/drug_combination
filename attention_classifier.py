3b2045abb8a6b639d328046ba4981c3e4533d0a4
QiaoLiu <smileqiao1988@yahoo.com>
2020-10-30 15:18:02 -0400

    debug

--- attention_classifier.py
+++ attention_classifier.py
@@ -24,9 +24,11 @@ import sys
 sys.path.append(path.dirname(path.realpath(__file__)) + '/NeuralFingerPrint')
 import data_utils
 import timeit
+import pdb
 
 USE_wandb = True
 if USE_wandb:
+    pdb.set_trace()
     wandb.init(project="Drug combination hyper",
                name = setting.run_dir + '_' + setting.data_specific[:30],
                notes = setting.data_specific)
